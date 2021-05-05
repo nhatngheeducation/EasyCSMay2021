98d6325d1a6cbde6d34a73f2a28ad4e2ac409961
Hien <aspcorenn@gmail.com>
2021-05-05 19:24:04 +0700

    Demo 01

---
+++ Buoi01/ViDuDauTien/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ViDuDauTien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Xin chào các bạn");
+            string HoTen;
+            Console.Write("Ten ban: ");
+            HoTen = Console.ReadLine();
+            int NamSinh;
+            Console.Write("Sinh: ");
+            NamSinh = int.Parse(Console.ReadLine());
+
+            var Tuoi = DateTime.Now.Year - NamSinh;
+
+            var chuoiXuat = $"Ban {HoTen} sinh {NamSinh}, {Tuoi} tuoi";
+            Console.WriteLine(chuoiXuat);
+        }
+    }
+}

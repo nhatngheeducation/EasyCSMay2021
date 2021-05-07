73dea5ba243c83f3f6dc373cdd0fd8091de29293
Hien <aspcorenn@gmail.com>
2021-05-07 20:51:12 +0700

    Nam Thang

---
+++ Buoi02_TinhDiemTrungBinh/KiemTraNamNhuan/frmMain.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace KiemTraNamNhuan
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtThang = new System.Windows.Forms.TextBox();
+            this.btnTinhToan = new System.Windows.Forms.Button();
+            this.chkLaNamNhuan = new System.Windows.Forms.CheckBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Năm";
+            // 
+            // txtNam
+            // 
+            this.txtNam.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNam.Location = new System.Drawing.Point(118, 13);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(100, 26);
+            this.txtNam.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tháng";
+            // 
+            // txtThang
+            // 
+            this.txtThang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThang.Location = new System.Drawing.Point(118, 45);
+            this.txtThang.Name = "txtThang";
+            this.txtThang.Size = new System.Drawing.Size(100, 26);
+            this.txtThang.TabIndex = 3;
+            // 
+            // btnTinhToan
+            // 
+            this.btnTinhToan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTinhToan.Location = new System.Drawing.Point(118, 77);
+            this.btnTinhToan.Name = "btnTinhToan";
+            this.btnTinhToan.Size = new System.Drawing.Size(75, 36);
+            this.btnTinhToan.TabIndex = 4;
+            this.btnTinhToan.Text = "Tính toán";
+            this.btnTinhToan.UseVisualStyleBackColor = true;
+            this.btnTinhToan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chkLaNamNhuan
+            // 
+            this.chkLaNamNhuan.AutoSize = true;
+            this.chkLaNamNhuan.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkLaNamNhuan.Location = new System.Drawing.Point(25, 113);
+            this.chkLaNamNhuan.Name = "chkLaNamNhuan";
+            this.chkLaNamNhuan.Size = new System.Drawing.Size(112, 23);
+            this.chkLaNamNhuan.TabIndex = 6;
+            this.chkLaNamNhuan.Text = "Là năm nhuận";
+            this.chkLaNamNhuan.UseVisualStyleBackColor = true;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.Location = new System.Drawing.Point(25, 149);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(200, 26);
+            this.txtKetQua.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 215);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.chkLaNamNhuan);
+            this.Controls.Add(this.btnTinhToan);
+            this.Controls.Add(this.txtThang);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtThang;
+        private System.Windows.Forms.Button btnTinhToan;
+        private System.Windows.Forms.CheckBox chkLaNamNhuan;
+        private System.Windows.Forms.TextBox txtKetQua;
+    }
+}
+
---
+++ Buoi02_TinhDiemTrungBinh/KiemTraNamNhuan/frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KiemTraNamNhuan
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DateTime Ngay = new DateTime(2100, 3, 1).AddDays(-1);
+            //MessageBox.Show(Ngay.ToString());
+
+            int Nam, Thang;
+            if (!int.TryParse(txtNam.Text, out Nam) || Nam < 1900)
+            {
+                MessageBox.Show("Năm không hợp lệ");
+                txtNam.Focus();
+                return;
+            }
+            if (!int.TryParse(txtThang.Text, out Thang) || Thang < 1 || Thang > 12)
+            {
+                MessageBox.Show("Tháng không hợp lệ");
+                txtThang.Focus();
+                return;
+            }
+            bool LaNamNhuan = false;
+            if (Nam % 400 == 0)
+            {
+                LaNamNhuan = true;
+            }
+            else if(Nam % 4 == 0 && Nam % 100 != 0)
+            {
+                LaNamNhuan = true;
+            }
+            int SoNgay = 0;
+            switch (Thang)
+            {
+                case 1: case 3: case 5: case 7:
+                case 8: case 10: case 12:
+                    SoNgay = 31; break;
+                case 2:
+                    SoNgay = LaNamNhuan ? 29 : 28;
+                    break;
+                default:
+                    SoNgay = 30; break;
+            }
+            chkLaNamNhuan.Checked = LaNamNhuan;
+            txtKetQua.Text = $"Tháng {Thang}/{Nam} có {SoNgay} ngày.";
+        }//end
+    }
+}

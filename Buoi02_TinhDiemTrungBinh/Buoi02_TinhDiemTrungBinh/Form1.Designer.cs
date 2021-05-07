bea628a6165b0de8a95b985f763e12d6b8e87e0b
Hien <aspcorenn@gmail.com>
2021-05-07 20:01:23 +0700

    Day02 - Tinh DIem Trung Binh

---
+++ Buoi02_TinhDiemTrungBinh/Buoi02_TinhDiemTrungBinh/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtToan = new System.Windows.Forms.TextBox();
+            this.txtLy = new System.Windows.Forms.TextBox();
+            this.txtHoa = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDTB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtXepLoai = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TÍNH ĐIỂM TRUNG BÌNH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Điểm Toán";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Điểm Lý";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Điểm Hóa";
+            // 
+            // txtToan
+            // 
+            this.txtToan.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtToan.Location = new System.Drawing.Point(134, 50);
+            this.txtToan.Name = "txtToan";
+            this.txtToan.Size = new System.Drawing.Size(100, 26);
+            this.txtToan.TabIndex = 4;
+            //this.txtToan.TextChanged += new System.EventHandler(this.txtToan_TextChanged);
+            // 
+            // txtLy
+            // 
+            this.txtLy.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLy.Location = new System.Drawing.Point(134, 81);
+            this.txtLy.Name = "txtLy";
+            this.txtLy.Size = new System.Drawing.Size(100, 26);
+            this.txtLy.TabIndex = 5;
+            // 
+            // txtHoa
+            // 
+            this.txtHoa.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoa.Location = new System.Drawing.Point(134, 118);
+            this.txtHoa.Name = "txtHoa";
+            this.txtHoa.Size = new System.Drawing.Size(100, 26);
+            this.txtHoa.TabIndex = 6;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTinh.Location = new System.Drawing.Point(134, 150);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(100, 30);
+            this.btnTinh.TabIndex = 7;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Điểm trung bình";
+            // 
+            // txtDTB
+            // 
+            this.txtDTB.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDTB.Location = new System.Drawing.Point(134, 192);
+            this.txtDTB.Name = "txtDTB";
+            this.txtDTB.Size = new System.Drawing.Size(100, 26);
+            this.txtDTB.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 232);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Xếp loại";
+            // 
+            // txtXepLoai
+            // 
+            this.txtXepLoai.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXepLoai.Location = new System.Drawing.Point(134, 232);
+            this.txtXepLoai.Name = "txtXepLoai";
+            this.txtXepLoai.Size = new System.Drawing.Size(100, 26);
+            this.txtXepLoai.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 290);
+            this.Controls.Add(this.txtXepLoai);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtHoa);
+            this.Controls.Add(this.txtLy);
+            this.Controls.Add(this.txtToan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtToan;
+        private System.Windows.Forms.TextBox txtLy;
+        private System.Windows.Forms.TextBox txtHoa;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDTB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtXepLoai;
+    }
+}
+
---
+++ Buoi02_TinhDiemTrungBinh/Buoi02_TinhDiemTrungBinh/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Lấy giá trị từ các textbox
+                //double Toan = double.Parse(txtToan.Text);
+                double Toan;
+                if (double.TryParse(txtToan.Text, out  Toan) == false || Toan < 0 || Toan > 10)
+                {
+                    MessageBox.Show("Điểm Toán không hợp lệ");
+                    txtToan.Focus();
+                    return;//ép kết thúc hàm tại đây
+                }
+
+                double Ly;
+                if (double.TryParse(txtLy.Text, out Ly) == false || Ly < 0 || Ly > 10)
+                {
+                    MessageBox.Show("Điểm Lý không hợp lệ");
+                    txtLy.Focus();
+                    return;//ép kết thúc hàm tại đây
+                }
+
+                double Hoa;
+                if (double.TryParse(txtHoa.Text, out Hoa) == false || Hoa < 0 || Hoa > 10)
+                {
+                    MessageBox.Show("Điểm Hóa không hợp lệ");
+                    txtHoa.Focus();
+                    return;//ép kết thúc hàm tại đây
+                }
+
+                double DiemTrungBinh = (Toan + Ly + Hoa) / 3;
+                string XepLoai = string.Empty;
+                if (DiemTrungBinh < 5)
+                {
+                    XepLoai = "Yếu";
+                }
+                //else if(DiemTrungBinh >= 5 && DiemTrungBinh < 7)
+                else if (DiemTrungBinh < 7)
+                {
+                    XepLoai = "Trung bình";
+                }
+                else if (DiemTrungBinh < 8)
+                {
+                    XepLoai = "Khá";
+                }
+                else if (DiemTrungBinh < 9)
+                {
+                    XepLoai = "Giỏi";
+                }
+                else
+                {
+                    XepLoai = "Xuất sắc";
+                }
+
+                txtDTB.Text = DiemTrungBinh.ToString();
+                txtXepLoai.Text = XepLoai;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
+
+
+
+        }// end sự kiện click
+
+
+    }
+}
